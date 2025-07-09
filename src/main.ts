6b952ddf29216ca35bb464fb8fb415cb6f5dec55
Marlo Eichenberg Motta <marlo.eichenberg@gmail.com>
2025-07-09 10:01:20 -0300

    feat: added partialType to update message DTO

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Automatically remove properties that do not have decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+      transform: true, // Automatically transform payloads to DTO instances
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();

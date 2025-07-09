8f4a8b0480ad0f1b8f11423395411bd67e9431eb
Marlo Eichenberg Motta <marlo.eichenberg@gmail.com>
2025-07-09 02:30:45 -0300

    feat: creating a module automatically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ManualConceptsModule } from '../manual-concepts/manual-concepts.module';
+import { AutomaticConceptsModule } from '../automatic-concepts/automatic-concepts.module';
 
 @Module({
-  imports: [ManualConceptsModule],
+  imports: [ManualConceptsModule, AutomaticConceptsModule],
   controllers: [AppController],
   providers: [AppService],
 })

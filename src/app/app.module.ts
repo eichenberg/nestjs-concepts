cc5a39dab18946a45e4c239257852c6333e4b083
Marlo Eichenberg Motta <marlo.eichenberg@gmail.com>
2025-07-17 01:34:00 -0300

    feat: database configuration and docker setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,26 @@ import { AppService } from './app.service';
 import { ManualConceptsModule } from '../manual-concepts/manual-concepts.module';
 import { AutomaticConceptsModule } from '../automatic-concepts/automatic-concepts.module';
 import { MessagesModule } from 'src/messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ManualConceptsModule, AutomaticConceptsModule, MessagesModule],
+  imports: [
+    ManualConceptsModule,
+    AutomaticConceptsModule,
+    MessagesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'mysql',
+      password: 'mysql',
+      database: 'concepts_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      logging: true,
+      autoLoadEntities: true, // Automatically load entities
+      synchronize: true, //Do not use in production, only for development
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

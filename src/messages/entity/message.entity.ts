042d5264d6a82bebeffa80fb8e709bc1ebb19d3b
Marlo Eichenberg Motta <marlo.eichenberg@gmail.com>
2025-07-17 01:53:15 -0300

    feat: create table Message from entity

--- src/messages/entity/message.entity.ts
+++ src/messages/entity/message.entity.ts
@@ -1,8 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class Message {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   content: string;
+
+  @Column({ type: 'varchar', length: 50 })
   from: string;
+
+  @Column({ type: 'varchar', length: 50 })
   to: string;
+
+  @Column({ type: 'boolean', default: false })
   isRead: boolean;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   date: Date;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
 }
